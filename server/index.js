82d5e7cf429a37cb56c2621e5717c324073a5e4f
Serhii Persianov <sergiy.persianov@gmail.com>
2019-03-25 17:55:50 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
-const app = require('express')();
 const cors = require('cors');
+const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 
 app.use(cors());
 
-
+io.on('connection', () => {
+    console.log('connected');
+});
 
 
 
