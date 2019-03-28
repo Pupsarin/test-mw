9666333ff80e176e0d0ea4159900b545ecf5f948
Serhii Persianov <sergiy.persianov@gmail.com>
2019-03-28 16:51:59 +0200

    router fix

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -5,22 +5,17 @@ import Chat from './Chat';
 import AuthForm from './AuthForm';
 import { authUser } from '../actions';
 
-const Main = (props) => {
-
-
-
-    return(
-        <Switch>
-            { !localStorage.chatToken && ( <Redirect to='/enter-chat' /> ) }
-            <Route path="/enter-chat" render={ () => (
-                <AuthForm 
-                auth={props.authUser}
-                {...props}
-                />
-                ) }/>
-            <Route exact path='/' render={() => { return(<Chat />) }}/>
-        </Switch>
-    )
-}
+const Main = (props) => (
+    <Switch>
+        <Route path="/enter-chat" render={ () => (
+            <AuthForm 
+            auth={props.authUser}
+            {...props}
+            />
+            ) }/>
+        { !localStorage.chatToken && ( <Redirect to='/enter-chat' /> ) }        
+        <Route exact path='/' render={() => { return(<Chat />) }}/>
+    </Switch>
+)
 
 export default withRouter(connect(null, { authUser })(Main));
