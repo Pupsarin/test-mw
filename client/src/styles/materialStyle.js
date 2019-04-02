6f0b071b0865a153b0fbc998c7455052e07f12e4
Serhii Persianov <sergiy.persianov@gmail.com>
2019-04-02 17:34:07 +0300

    fix signin page alignment

--- client/src/styles/materialStyle.js
+++ client/src/styles/materialStyle.js
@@ -81,6 +81,8 @@ const materialStyle = theme => ({
         },
     paper: {
         marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
         padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
     },
