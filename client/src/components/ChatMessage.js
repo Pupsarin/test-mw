78e4867b944c1b025615881ce1094a8556c156a3
Serhii Persianov <sergiy.persianov@gmail.com>
2019-04-02 17:25:49 +0300

    add style to chat message component

--- client/src/components/ChatMessage.js
+++ client/src/components/ChatMessage.js
@@ -1,15 +1,29 @@
 
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import materialStyle from '../styles/materialStyle';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 class ChatMessage extends Component {
     render(){
-        const {message, username, usernameColor} = this.props;
+        const {message, username, usernameColor, classes} = this.props;
         return (
-            <div className="chat-message">
-                <p><strong style={{color: usernameColor}}>{username}: </strong>{message}</p>
-            </div>
+                <Card style={{backgroundColor: usernameColor}} className={classes.card}>
+                    <CardContent className={classes.cardContent}>
+                        <Typography className={classes.messageText} gutterBottom>
+                            {username}
+                        </Typography>
+                        <Typography className={classes.messageText} variant="h5" component="h2">
+                            {message}
+                        </Typography>
+                    </CardContent>
+                </Card>
         )
     }
 }
 
-export default ChatMessage;
+export default withStyles(materialStyle)(ChatMessage);
+
+
--- client/src/styles/materialStyle.js
+++ client/src/styles/materialStyle.js
@@ -38,7 +38,26 @@ const materialStyle = theme => ({
 
     },
     chatMessages: {
-        paddingBottom: appBarHeight + 5 
+        paddingBottom: appBarHeight + 5,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        maxWidth: '100%'
+    },
+    card: {
+        margin: '5px 5px 5px -10px',
+    },
+    cardContent: {
+        '&:last-child': {
+            paddingBottom: 5
+        },
+        padding: 10,
+    },
+    messageText: {
+        color: '#fff',
+        whiteSpace: 'pre-wrap',
+        overflowWrap: 'break-word',
     },
     messageInput: {
         width: '100%'
@@ -49,47 +68,33 @@ const materialStyle = theme => ({
             display: 'none',
         },
     },
-
     authPage: {
-        width: 'auto',
-        display: 'block', // Fix IE 11 issue.
-        marginLeft: theme.spacing.unit * 3,
-        marginRight: theme.spacing.unit * 3,
-        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-          width: 400,
-          marginLeft: 'auto',
-          marginRight: 'auto',
+            width: 'auto',
+            display: 'block',
+            marginLeft: theme.spacing.unit * 3,
+            marginRight: theme.spacing.unit * 3,
+            [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+                width: 400,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+            },
         },
-      },
-      paper: {
+    paper: {
         marginTop: theme.spacing.unit * 8,
-        display: 'flex',
-        flexDirection: 'column',
         alignItems: 'center',
         padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-      },
-      avatar: {
+    },
+    avatar: {
         margin: theme.spacing.unit,
         backgroundColor: theme.palette.secondary.main,
-      },
-      form: {
-        width: '100%', // Fix IE 11 issue.
+    },
+    form: {
+        width: '100%',
         marginTop: theme.spacing.unit,
-      },
-      submit: {
+    },
+    submit: {
         marginTop: theme.spacing.unit * 3,
-      },
-
-
-
-
-
-
-
-
-
-
-
+    },
 })
 
 export default materialStyle;
